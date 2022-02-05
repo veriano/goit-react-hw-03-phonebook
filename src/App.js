f91bc3a6565015793d3fa337e997607f271b57f5
veriano <86019033+veriano@users.noreply.github.com>
2022-02-05 20:50:45 +0200

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import Filter from './Components/Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
+    ],
     filter: "",
   }
 
@@ -60,9 +65,7 @@ class App extends Component {
     const { contacts, filter } = this.state;
     const normolizedFilter = filter.toLowerCase();
 
-    if(contacts !== null) {
     return contacts.filter(contact => contact.name.toLowerCase().includes(normolizedFilter));
-    }
   }
     
 
